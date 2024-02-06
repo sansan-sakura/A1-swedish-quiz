41d0941692dba134e359c02dd30c9a5c5032cb13
Sakura Tanaka <sito6496t@gmail.com>
2024-02-07 00:04:49 +0100

    Add json

--- src/components/CurrentQuestionZustand.jsx
+++ src/components/CurrentQuestionZustand.jsx
@@ -2,9 +2,7 @@ import useQuizStore from "../stores/useQuizStore"; // Adjust the path accordingl
 
 export const CurrentQuestionZustand = () => {
   const questions = useQuizStore((state) => state.questions);
-  const currentQuestionIndex = useQuizStore(
-    (state) => state.currentQuestionIndex
-  );
+  const currentQuestionIndex = useQuizStore((state) => state.currentQuestionIndex);
   const question = questions[currentQuestionIndex];
 
   if (!question) {
@@ -12,9 +10,18 @@ export const CurrentQuestionZustand = () => {
   }
 
   return (
-    <div className="managed-component">
-      <h2>Using Zustand</h2>
-      <h1>Question: {question.questionText}</h1>
+    <div>
+      <h2 className="text-3xl font-semibold">Test Your Swedish 🇸🇪</h2>
+      <h1 className="text-xl ">Question: {question.question}</h1>
+      <div>
+        <ul className="flex gap-10">
+          {question?.choices?.map((choice) => (
+            <li key={choice} className="">
+              <button className="">{choice}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/stores/useQuizStore.js
+++ src/stores/useQuizStore.js
@@ -1,54 +1,37 @@
 import { create } from "zustand";
 
-const questions = [
-  {
-    id: 1,
-    questionText: "Who set the Olympic record for the 100m dash in 2012?",
-    options: ["Usain Bolt", "Justin Gatlin", "Tyson Gay", "Asafa Powell"],
-    correctAnswerIndex: 0,
-  },
-  {
-    id: 2,
-    questionText:
-      "When was Michael Phelps last named male World Swimmer of the Year?",
-    options: ["2012", "2014", "2016", "2018"],
-    correctAnswerIndex: 2,
-  },
-];
+import data from "../static/data.json";
 
 const useQuizStore = create((set) => ({
-  questions,
+  questions: data.questions,
   answers: [],
   currentQuestionIndex: 0,
   quizOver: false,
 
   submitAnswer: (questionId, answerIndex) => {
-    const question = questions.find((q) => q.id === questionId);
-
-    if (!question) {
-      throw new Error(
-        "Could not find question! Check to make sure you are passing the question id correctly."
-      );
-    }
-
-    if (question.options[answerIndex] === undefined) {
-      throw new Error(
-        `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
-      );
-    }
-
-    set((state) => ({
-      answers: [
-        ...state.answers,
-        {
-          questionId,
-          answerIndex,
-          question,
-          answer: question.options[answerIndex],
-          isCorrect: question.correctAnswerIndex === answerIndex,
-        },
-      ],
-    }));
+    // const question = questions.find((q) => q.id === questionId);
+    // if (!question) {
+    //   throw new Error(
+    //     "Could not find question! Check to make sure you are passing the question id correctly."
+    //   );
+    // }
+    // if (question.options[answerIndex] === undefined) {
+    //   throw new Error(
+    //     `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
+    //   );
+    // }
+    // set((state) => ({
+    //   answers: [
+    //     ...state.answers,
+    //     {
+    //       questionId,
+    //       answerIndex,
+    //       question,
+    //       answer: question.options[answerIndex],
+    //       isCorrect: question.correctAnswerIndex === answerIndex,
+    //     },
+    //   ],
+    // }));
   },
 
   goToNextQuestion: () => {
